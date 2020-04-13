2f63437e95b4c588d479d829682b829def6ec026
Ze'ev Gebler <zgebler@gmail.com>
2020-04-13 15:37:11 -0400

    updating files

--- Code/config.py
+++ Code/config.py
@@ -6,10 +6,10 @@ description:  configuration file for kalamatchkas program
 
 
 # file of foods to load
-FOOD_PATH = "C:/Users/Doron/Desktop/Rehab/kalamatchkas/ingredient_doron v21.xlsx"
+FOOD_PATH = "C:/Users/zgebl/Documents/dsi/GitHub/Kalamatchkas/Assets/ingredient_doron v21.xlsx"
 
 # file to output recipes
-OUT_DIREC = "C:/Users/Doron/Desktop/Rehab/Journals/Weekly Menus/preliminary k outputs"
+OUT_DIREC = "C:/Users/zgebl/Documents/dsi/GitHub/Kalamatchkas/Output"
 
 # file for food list
 FOOD_LIST = "C:/Users/Doron/Desktop/Rehab/kalamatchkas/kalamachkas_project_files/meal_creation_files/food_list individual meals.csv"
@@ -24,8 +24,7 @@ BASE_FIELDS = ["protein_cal_%","carb_cal_%","fat_cal_%","total_cal"]
 FIELDS_DONT_UPDATE = ['food_id','serving_size','max_grams_meal','max_grams_day']
 
 # api key for searching usda database
-API_KEY = "xHAJ4pkp29k08lmA0vW554IxhtoR1IRekdCkPpLL" #Meir: 
-"Gb3vhyvcHwFhZNtbD2fqWc3oeNdwYA22qqND1fyU"
+API_KEY = "NzigPCefzEU9ewreg7xHSR5E1oW9xWVZQlX5vNEs"
 
 # debug mode
 DEBUG = False
